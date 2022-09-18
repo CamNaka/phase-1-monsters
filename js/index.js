8af53fac41585915f8db7c897e72c021414a36e2
Cameron <cameronnakaba4@gmail.com>
2022-09-18 09:32:14 -0700

    pulling data from server, creating new monster data

--- js/index.js
+++ js/index.js
@@ -1,9 +1,144 @@
+
+fetchMonster()
+createForm()
+document.querySelector('#monster-form').addEventListener('submit', (event) => {
+  event.preventDefault()
+
+  let name = document.getElementById('name-Id').value 
+  let age = document.getElementById('age-Id').value 
+  let description = document.getElementById('descript-Id').value 
+
+  monsterObj = {
+    name, 
+    age,
+    description
+  }
+  console.log(monsterObj)
+
+postNewMonster(monsterObj)
+  
+})
+
+
+
+// /?_limit=50&_page=1
+
+//fetch from http://localhost:3000/monsters
+//show forEach monster age, name and description
+//show data
+
+
+function fetchMonster() {
+  const monsterContainer = document.getElementById('monster-container')
+  fetch('http://localhost:3000/monsters/?_limit=50&_page=1')
+    .then(resp => resp.json())
+    .then(monsterData => {
+      console.log(monsterData)
+      monsterData.forEach((monster) => {
+
+        let card = document.createElement('div')
+        let name = document.createElement('h2')
+        let age = document.createElement('h4')
+        let description = document.createElement('p')
+
+        name.innerText = `Name: ${monster.name}`
+        age.innerText = `Age: ${monster.age}`
+        description.innerText = `Bio: ${monster.description}`
+
+        card.append(name, age, description)
+        monsterContainer.append(card)
+      })
+    })
+}
+
+function createForm() {
+  const formContainer = document.getElementById('create-monster')
+  const form = document.createElement('form')
+
+  form.id = 'monster-form'
+
+  const nameInput = document.createElement('input')
+  const nameId = nameInput.setAttribute('id', 'name-Id')
+  nameInput.setAttribute('placeholder', 'Name...')
+
+  const ageInput = document.createElement('input')
+  const ageId = ageInput.setAttribute('id', 'age-Id')
+  ageInput.setAttribute('placeholder', 'Age...')
+
+  const descriptionInput = document.createElement('input')
+  const descriptId = descriptionInput.setAttribute('id', 'descript-Id')
+  descriptionInput.setAttribute('placeholder', 'Description...')
+
+  const createButton = document.createElement('button')
+  createButton.textContent = 'Create'
+
+  
+
+  form.append(nameInput, ageInput, descriptionInput, createButton)
+  formContainer.append(form)
+}
+
+
+function postNewMonster(name, age, description) {
+  fetch('http://localhost:3000/monsters', {
+    method: 'POST',
+    header: {
+    "Content-Type": "application/json",
+    Accept: "application/json"
+    },
+
+    body: {
+      name, age, description
+    }
+
+  })
+  .then(resp => resp.json())
+  .then(monster => console.log(monster))
+    
+}
+
+
+/*
+POST http://localhost:3000/monsters
+headers:
+{
+  "Content-Type": "application/json",
+  Accept: "application/json"
+}
+
+body:
+{ name: string, age: number, description: string }
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
 nameInput()
 innerName()
 innerAge()
 innerDescript()
 buttonGrab()
 createButton()
+
+
+
+
 
 
 function nameInput() {
@@ -46,7 +181,13 @@ function createButton() {
   const grabInnerId = document.getElementById('inner-monster')
   createBut.textContent = 'Create'
   grabInnerId.appendChild(createBut)
+}
 
+function buttonGrab() {
+  const forwardBut = document.getElementById('button-Id');
+  const event = forwardBut.addEventListener('submit', () => {
+    console.log('empty submit')
+  })
 }
 
 
@@ -63,3 +204,7 @@ function buttonGrab() {
     alert('Aint no monsters here')
   })
 }
+
+
+
+*/
